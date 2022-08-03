14eb7c6852d015d33b5f2651fda0fdb2756be295
Tenshi Kira <garciarodrigueza587@gmail.com>
2022-08-03 15:38:22 -0600

    mantenimiento V2.1

--- SaturIp.py
+++ SaturIp.py
@@ -15,7 +15,7 @@ import requests
 #limpio consola
 os.system("clear")
 
-print(""" 
+print(Fore.CYAN +""" 
                      .::.
                   .:'  .:
         ,MMM8&&&.:'   .:'
@@ -27,11 +27,21 @@ print("""
 .:'   .:'MMM8&&&'
 :'  .:'
 '::'  T3NSHI
+SaturIp
 
 """)
+azul = Fore.BLUE
+rojo = Fore.RED
+verde = Fore.GREEN
+magenta = Fore.MAGENTA
+negro =  Fore.BLACK
+back = Back.BLACK
+styl = Style.BRIGHT
 
+time.sleep(4.0)
+#>/dev/null &
 #el input donde ira la ip victima
-ip = input(str(Style.BRIGHT + Fore.RED + "Ingresa la Ip: " + Fore.GREEN ))
+ip = input(str(styl + rojo + "Ingresa la Ip: " + verde ))
 dirección_ip = ip;
 #se guarda en la variable 
 
@@ -41,9 +51,10 @@ url = 'https://ipfind.co/?auth=' + autenticacion + '&ip=' + dirección_ip;
 pedir = requests.get(url)
 respuesta = json.loads(pedir.content)
 
-print("\n")
-
-Pais = Style.BRIGHT + Fore.BLUE + "La Victima Es De: " + Fore.GREEN + respuesta['country']
+os.system("mpv optenidos.mp3 >/dev/null &");
+time.sleep(6.1)
+print(" ")
+Pais = styl + azul + "La Victima Es De: " + verde + respuesta['country']
 for l in Pais:
     sys.stdout.flush()
     print(l,end="")
@@ -52,7 +63,7 @@ for l in Pais:
 print("\n")
 time.sleep(0.1)
 
-Continen = Style.BRIGHT + Fore.BLUE + "Continente de: " + Fore.GREEN + respuesta['continent']
+Continen = styl + azul + "Continente de: " + verde + respuesta['continent']
 for l in Continen:
     sys.stdout.flush()
     print(l,end="")
@@ -61,21 +72,21 @@ for l in Continen:
 print("\n")
 time.sleep(0.1)
 
-Ciuda = Style.BRIGHT , Fore.BLUE , "De La Ciudad De: " , Fore.GREEN , respuesta['city']
+Ciuda = styl , azul , "De La Ciudad De: " , verde , respuesta['city']
 for l in Ciuda:
     sys.stdout.flush()
     print(l,end="")
     time.sleep(0.1)
 
+
 longitud = respuesta['longitude']
 latitud =  respuesta['latitude']
 
+#os.system("mpv cordenadas.mp3 >/dev/null &");
+
 print("\n")
-
-print(Style.BRIGHT + Fore.RED + "La ultima Localizacion GPS Fue")
-
-cordenadas = Style.BRIGHT , Fore.BLUE, "Latitud " , Fore.GREEN , latitud , Fore.BLUE , " Longitude " , Fore.GREEN , longitud
-
+#print(Style.BRIGHT + Fore.RED + "La ultima Localizacion GPS Fue")
+cordenadas = styl , azul , "Latitud " , verde , latitud , azul , " Longitude " , verde , longitud
 
 for l in cordenadas:
     sys.stdout.flush()
@@ -83,11 +94,11 @@ for l in cordenadas:
     time.sleep(1.0)
 
 print("\n")
-print(Style.BRIGHT + Fore.BLUE + "Locacion > " , Fore.GREEN , latitud ,  longitud )
+print(styl + azul + "Locacion > " , verde , latitud ,  longitud )
 print("\n")
 time.sleep(1.0)
 
-Region = Style.BRIGHT , Fore.BLUE , "Codigo de Region: " , Fore.GREEN , respuesta['region_code']
+Region = styl , azul , "Codigo de Region: " , verde , respuesta['region_code']
 
 for l in Region:
     sys.stdout.flush()
@@ -97,7 +108,7 @@ for l in Region:
 print("\n")
 time.sleep(0.1)
 
-Postal = Style.BRIGHT , Fore.BLUE , "Codigo Postal: " , Fore.GREEN , respuesta['postal_code']
+Postal = styl , azul , "Codigo Postal: " , verde , respuesta['postal_code']
 
 for l in Postal:
     sys.stdout.flush()
@@ -107,7 +118,7 @@ for l in Postal:
 print("\n")
 time.sleep(0.1)
 
-lenguaje = Style.BRIGHT , Fore.BLUE , "Idioma: " , Fore.GREEN , respuesta['languages']
+lenguaje = styl , azul , "Idioma: " , verde , respuesta['languages']
 
 for l in lenguaje:
     sys.stdout.flush()
@@ -126,7 +137,7 @@ for l in nada:
 
 time.sleep(0.1)
 
-print(Style.BRIGHT , Fore.BLUE + "Moneda: " , Fore.GREEN , respuesta['currency'])
+print(styl , azul + "Moneda: " , verde , respuesta['currency'])
 
 
 
